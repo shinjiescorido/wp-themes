fd49d495995f33757702bfd45ec5dd2588a89359
shinjiescorido <shinji.escorido@gmail.com>
2015-08-12 22:34:53 -0400

    parsed out theme ads

--- pinbin/footer.php
+++ pinbin/footer.php
@@ -12,7 +12,6 @@
 <?php }  ?>           
 <footer class="site-footer">
 	 <div id="copyright">
-	 	<?php _e( 'Pinbin Theme by', 'pinbin' ); ?> <a href="<?php echo esc_url( 'http://colorawesomeness.com/themes/' ); ?>" title="Color Awesomeness" target="_blank"><?php _e( 'Color Awesomeness', 'pinbin' ); ?></a> | 
 		<?php _e( 'Copyright', 'pinbin' ) ?> <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?> |
 		<?php _e( 'Powered by' , 'pinbin' ); ?> <a href="http://www.wordpress.org" target="_blank" title="<?php _e( 'Powered by WordPress' , 'pinbin' ); ?>">WordPress</a>
 	 </div><!-- // copyright -->   
